30277ef2409e435ec62f6e8f0293942f110bd824
knudtraphillers <86996911+knudtraphillers@users.noreply.github.com>
2021-07-09 14:44:42 +0200

    Update app.js

--- comp/assets/js/app.js
+++ comp/assets/js/app.js
@@ -231,6 +231,24 @@ var Kartverket2 = L.tileLayer.wms("https://openwms.statkart.no/skwms1/wms.topo3.
     // attribution: "Kartverket.no"
 });
 
+var Kartverket3_stedsnavn = L.tileLayer.wms("https://openwms.statkart.no/skwms1/wms.topo4.graatone?", {
+    layers: 'Stedsnavn',
+    format: 'image/png',
+    transparent: true,
+    version: '1.1.1',
+    // attribution: "Kartverket.no"
+});
+
+// var kartverket_stednavn = L.tileLayer.wms("https://openwms.statkart.no/skwms1/wms.topo3.graatone?", {
+//     layers: 'Tekst,Stedsnavn',
+//     format: 'image/png',
+//     transparent: true,
+//     opacity: 0.99,
+// 	zIndex: 3,
+// 	version: '1.1.1',
+//   attribution: "Kartverket.no"
+// });
+
 var AR50 = L.tileLayer.wms("https://wms.nibio.no/cgi-bin/ar50?", {
     layers: 'Skogbonitet',
     format: 'image/png',
@@ -252,16 +270,6 @@ var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/
 {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-
-// var kartverket_stednavn = L.tileLayer.wms("https://openwms.statkart.no/skwms1/wms.topo3.graatone?", {
-//     layers: 'Tekst,Stedsnavn',
-//     format: 'image/png',
-//     transparent: true,
-//     opacity: 0.99,
-// 	zIndex: 3,
-// 	version: '1.1.1',
-//   attribution: "Kartverket.no"
-// });
 
 // var imagery_placenames = L.layerGroup([
 //
