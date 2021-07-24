c7fc2443abd98635a4abfdb6726e713cdd167c6e
maps4u <86996911+maps4u@users.noreply.github.com>
2021-07-24 17:29:49 +0200

    Update app.js

--- ontour/assets/js/app.js
+++ ontour/assets/js/app.js
@@ -117,7 +117,7 @@ function clearHighlight() {
 function sidebarClick(id) {
   var layer = markerClusters.getLayer(id);
   map.setView([layer.getLatLng().lat, layer.getLatLng().lng], layer.feature.properties.zoom); //her kontrollerer du zoom-nivå ved klikk på objekt i sidebar
-  layer.fire("click"); // skru denne på viss du vil opne dialogen når du klikker, eller om du berre vil zoome til POI
+  /*layer.fire("click"); // skru denne på viss du vil opne dialogen når du klikker, eller om du berre vil zoome til POI */
   /* Hide sidebar and go to the map on small screens */
   if (document.body.clientWidth <= 767) {
     $("#sidebar").hide();
